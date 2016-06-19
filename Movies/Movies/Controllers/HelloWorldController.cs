aa9aab9a3f8deb8cba0d9ff7e793c27bb145b6da
Rangel Marinov <rangelvmarinov@gmail.com>
2016-06-19 17:33:33 +0300

    HelloWorldController created.

---
+++ Movies/Movies/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public string Index()
+        {
+            //return View();
+            return "Hello World!"; 
+        }
+
+        public string Welcome(string name, int numTimes = 0, int age = 18)
+        {
+            return String.Format("Welcome {0} !!! {1}. My age is {2}.", name, numTimes, age);
+        }
+    }
+}
